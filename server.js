f3e3bae71fe4b94b06733e55b9a433ff02e1d7c8
AbdelrahmanMo111 <abdo.aboerba@gmail.com>
2024-04-25 10:54:04 +0200

    first commit

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ const app = express();
 app.use(cors());
 app.options("*", cors());
 app.use(compression());
-
+app.post(
+  "/webhook-checkout",
+  express.raw({ type: "application/json" }),
+  webhook
+);
 app.use(express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 
@@ -34,11 +38,6 @@ const PORT = process.env.PORT || 8000;
 const listen = app.listen(PORT, () => {
   console.log(`server is runing at ${PORT}`);
 });
-app.post(
-  "/webhook-checkout",
-  express.raw({ type: "application/json" }),
-  webhook
-);
 
 confirmrout(app);
 
